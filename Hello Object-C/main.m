b739ddecdc403e64025ce332ca3c13d93462ebda
黄立光 <huangliguang@huangliguangdemac.local>
2016-04-07 21:00:03 -0700

    Initial Commit

---
+++ Hello Object-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Hello Object-C
+//
+//  Created by 黄立光 on 16/4/7.
+//  Copyright © 2016年 黄立光. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
