39fbe6c1b0e750e0a0253c9ec44636ea9adc52af
szuhlg <879830132@qq.com>
2017-07-21 12:53:15 +0800

    1

--- Hello Object-C/main.m
+++ Hello Object-C/main.m
@@ -9,9 +9,23 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
+    //@autoreleasepool{
         // insert code here...
-        NSLog(@"Hello, World!");
-    }
+        //if(argc==1){
+           // NSLog(@"you need to provide a file name");
+            //return (1);
+      //  }
+        FILE *wordFile = fopen("/tmp/word.txt","r");
+        char word[100];
+    
+        while (fgets(word, 100, wordFile)) {
+            word[strlen(word)-1]='\0';
+            
+            NSLog(@"%s is %d characters long", word, strlen(word));
+        }
+    fclose(wordFile);
+    NSString *string1;
+    string1=[NSString stringWithFormat:@"Hello%d",5];
     return 0;
+    //}
 }
